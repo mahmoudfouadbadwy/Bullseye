0f338f04134db230383b6073f1b667354633fbc7
Mahmoud Fouad <mahmoudfouad443@gmail.com>
2023-09-30 18:28:39 +0300

    - UI update

--- Bullseye/Home/ContentView.swift
+++ Bullseye/Home/ContentView.swift
@@ -19,58 +19,65 @@ struct ContentView: View {
     
     //MARK: - Body
     var body: some View {
-        VStack {
-            
-            Spacer()
-            TargetView(targetVal: self.targetValue)
-            
-            Spacer()
-            SliderView(sliderVal: $sliderValue)
-            
-            Spacer()
-            BullseyeButton(title: "Hit me!") {
-                self.alertIsVisible = true
-            }
-    
-            Spacer()
-            HStack {
-                BullseyeButton(title: "Start Over"){
-                    self.startNewGame()
+        NavigationView {
+            GeometryReader { geo in
+                VStack {
+                    TargetView(targetVal: self.targetValue)
+                    
+                    Spacer()
+                    SliderView(sliderVal: $sliderValue)
+                        .padding(10)
+                    
+                    Spacer()
+                    BullseyeButton(title: "Hit me!") {
+                        self.alertIsVisible = true
+                    }
+                    
+                    Spacer()
+                    HStack {
+                        BullseyeButton(title: "Start Over"){
+                            self.startNewGame()
+                        }
+                        Spacer()
+                        StaticText(txt: "Score:")
+                        DynamicText(txt: "\(self.score)")
+                        
+                        Spacer()
+                        StaticText(txt: "Round:")
+                        
+                        DynamicText(txt: "\(self.roundNo)")
+                        
+                        Spacer()
+                        NavigationLink(destination: AboutView()) {
+                            Text("Info")
+                                .modifier(ButtonLargeTextStyle())
+                        }
+                        .modifier(ButtonStyle())
+                        
+                    }
+                    .padding(.all, 20)
+                    
                 }
-                Spacer()
-                StaticText(txt: "Score:")
-                DynamicText(txt: "\(self.score)")
-               
-                Spacer()
-                StaticText(txt: "Round:")
-               
-                DynamicText(txt: "\(self.roundNo)")
-                
-                Spacer()
-                NavigationLink(destination: AboutView()) {
-                        Text("Info")
-                            .modifier(ButtonLargeTextStyle())
+                .padding(.vertical, geo.size.height * 0.2)
+                .frame(width: geo.size.width, height: geo.size.height)
+                .background(Color(.brown))
+                .padding(.vertical, 10)
+                .alert(isPresented: $alertIsVisible) {
+                    Alert(title: Text(alertTitle()),
+                          message: Text("The slider's value is \(self.getRoundedValue()). \n" +
+                                            "and your score = \(self.getCurrentRoundScore())"),
+                          dismissButton: .default(Text("Awesome!")) {
+                            self.score += self.getCurrentRoundScore()
+                            self.targetValue = Int.random(in: 1...100)
+                            self.roundNo += 1
+                          }
+                    )
                 }
-                 .modifier(ButtonStyle())
                 
             }
-            .padding(.all, 20)
-            
+            .edgesIgnoringSafeArea(.vertical)
         }
-        .background(Color(.brown))
         .navigationBarTitle("Bullseye")
-        .navigationBarTitleDisplayMode(.inline)
-        .alert(isPresented: $alertIsVisible) {
-            Alert(title: Text(alertTitle()),
-                  message: Text("The slider's value is \(self.getRoundedValue()). \n" +
-                                    "and your score = \(self.getCurrentRoundScore())"),
-                  dismissButton: .default(Text("Awesome!")) {
-                    self.score += self.getCurrentRoundScore()
-                    self.targetValue = Int.random(in: 1...100)
-                    self.roundNo += 1
-                  }
-            )
-        }
     }
     
     private func getCurrentRoundScore() -> Int {
